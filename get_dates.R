8d2e3b47eedf77e495edcea5c52cf2b1e0decea4
TomKellyGenetics <tomkellygenetics@gmail.com>
2023-08-07 14:22:43 +0900

    create dates

--- get_dates.R
+++ get_dates.R
@@ -18,25 +18,98 @@ days <- c("Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun")
 ii <- match(day_of_the_week, days)
 print(paste("today is", days[ii]))
 jan1_day <- format(as.Date(paste0(c(current_year, "01-01"), collapse="-")), "%a", tz = "UTC")
-ii <- match(jan1_day, days)
-print(paste("January 1st,", current_year, "was", days[ii]))
+jj <- match(jan1_day, days)
+print(paste("January 1st,", current_year, "was", days[jj]))
 
 months <- c("Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec")
-jj <- match(this_month, months)
-print(paste("today is", months[jj]))
+kk <- match(this_month, months)
+print(paste("today is", months[kk]))
 
-if(as.numeric(this_year) %% 4 == 0){
+if(as.numeric(current_year) %% 4 == 0){
   leap_year = TRUE
 } else {
   leap_year = FALSE
 }
-if(as.numeric(this_year) %% 100 == 0){
+if(as.numeric(current_year) %% 100 == 0){
   leap_year = TRUE
 }
-if(as.numeric(this_year) %% 400 == 0){
+if(as.numeric(current_year) %% 400 == 0){
   leap_year = FALSE
 }
 
 print(paste("leap year:", leap_year))
 month_length <- c(31, ifelse(leap_year, 29, 28), 31, 30, 31, 30, 31, 31, 30, 31, 30, 31)
+months_rep <- rep(months, month_length)
 
+filename <- paste0(current_year, ".txt")
+system(paste("touch", filename))
+system(paste("rm -rf", filename))
+system(paste("touch", filename))
+
+
+for(dd in 1:ifelse(leap_year, 366, 365)){
+    print(dd)
+    aa <- dd %% 7
+    if(aa == 0) aa <- 7
+    print(aa)
+    print(days[aa])
+    print(months_rep[dd])
+    print(length(grep(months_rep[dd], months_rep[1:dd])))
+    print(
+      paste(
+        days[aa],
+        months_rep[dd],
+        length(grep(months_rep[dd], months_rep[1:dd])),
+        "00:00:00",
+        current_year,
+        "+0000"
+      )
+    )
+   print(
+      paste(
+        "echo",
+        days[aa],
+        months_rep[dd],
+        length(grep(months_rep[dd], months_rep[1:dd])),
+        "00:00:00",
+        current_year,
+        "+0000",
+        "R",
+        filename
+      )
+    )
+}
+
+
+for(dd in 1:ifelse(leap_year, 366, 365)){
+    print(dd)
+    aa <- dd %% 7
+    if(aa == 0) aa <- 7
+    print(aa)
+    print(days[aa])
+    print(months_rep[dd])
+    print(length(grep(months_rep[dd], months_rep[1:dd])))
+    print(
+      paste(
+        days[aa],
+        months_rep[dd],
+        length(grep(months_rep[dd], months_rep[1:dd])),
+        "00:00:00",
+        current_year,
+        "+0000"
+      )
+    )
+   system(
+      paste(
+        "echo",
+        days[aa],
+        months_rep[dd],
+        length(grep(months_rep[dd], months_rep[1:dd])),
+        "00:00:00",
+        current_year,
+        "+0000",
+        ">>",
+        filename
+      )
+    )
+}
