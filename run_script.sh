7632480af2298a6b13d62117bedc82e32589610d
TomKellyGenetics <tomkellygenetics@gmail.com>
2023-08-07 15:08:07 +0900

    wrapper script fixed

---
+++ run_script.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+
+if [[ -z $1 ]]; then
+  year=$(date +%Y)
+fi
+
+Rscript get_dates.R $year
+
+sed "s/^/echo ' ' >> file\n git add file\n/g"  ${year}.commits.txt > ${year}.commits.sh
+
+bash ${year}.commits.sh
